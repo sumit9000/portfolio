7e6fb97111933a02e8cd01ba76e43da719859dee
Pranay <pranay0923@gmail.com>
2025-09-04 00:01:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sumit-portfolio',                    // Set base if deploying to subpath like GitHub Pages
+  base: '/Portfolio',                    // Set base if deploying to subpath like GitHub Pages
   assetsInclude: ['**/*.glb'],            // Allow importing .glb model files as modules
 });
